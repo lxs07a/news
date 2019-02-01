4cee338686d250430e93e44e1abebe300a58665d
Lyuda <lxs07a@acu.edu>
2019-02-01 09:05:31 +0100

    resolved router

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 // import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	// <BrowserRouter>
+		<App />,
+	// </BrowserRouter>,
 	document.getElementById('root')
 )
 
